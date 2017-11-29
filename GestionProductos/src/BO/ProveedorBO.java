b54fb2f4c6ee3f9e91b2b03c26a17f363cbd4b42
aaronjesus25 <aaronjesus25@gmail.com>
2017-11-28 22:45:39 -0600

    Proyecto Inventario
    última modificación

---
+++ GestionProductos/src/BO/ProveedorBO.java
@@ -0,0 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BO;
+
+/**
+ *
+ * @author aaron
+ */
+public class ProveedorBO {
+
+    
+    private int id;
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+    private String rfc;
+    private String direccion;
+    private String telefono;
+    private String celular;
+    private String Email;
+    
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellidoP
+     */
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    /**
+     * @param apellidoP the apellidoP to set
+     */
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    /**
+     * @return the apellidoM
+     */
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    /**
+     * @param apellidoM the apellidoM to set
+     */
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    /**
+     * @return the rfc
+     */
+    public String getRfc() {
+        return rfc;
+    }
+
+    /**
+     * @param rfc the rfc to set
+     */
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the celular
+     */
+    public String getCelular() {
+        return celular;
+    }
+
+    /**
+     * @param celular the celular to set
+     */
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    /**
+     * @return the Email
+     */
+    public String getEmail() {
+        return Email;
+    }
+
+    /**
+     * @param Email the Email to set
+     */
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+   
+}
